b5908f45d5e69c04f78a2fc07215ced952b666f1
cuongdt <duongthaicuong@gmail.com>
2022-02-05 13:13:00 +0700

    new: myDialog

---
+++ src/myDialog.spec.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from "@testing-library/react"
+import '@testing-library/jest-dom/extend-expect'
+
+import MyDialog from './myDialog'
+
+describe('MyDialog', () => {
+  it('should wrap child text', () => {
+    render(<MyDialog>Hello</MyDialog>)
+    expect(screen.getByRole('textbox')).toHaveTextContent('Hello')
+  })
+})
---
+++ src/myDialog.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dialog: React.FunctionComponent = ({ children }) => {
+  return <div role="textbox">{children}</div>;
+};
+
+export default Dialog
